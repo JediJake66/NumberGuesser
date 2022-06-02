dbc9aa7472380af276ecd246f72d8d2bb518cf71
JediJake66 <jacobstout59@gmail.com>
2022-06-02 15:13:00 -0400

    added try and catch to input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,31 +3,39 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        int randomNumber= ThreadLocalRandom.current().nextInt(1, 20 + 1);;
+        int randomNumber= ThreadLocalRandom.current().nextInt(1, 20 + 1);
         int guess=0;
         System.out.println("I'm thinking of a number between 1-20");
         System.out.println("Can you guess it in 6 tries?");
         Scanner getInput = new Scanner(System.in);
-        int input = getInput.nextInt();
-        while (input!=randomNumber) {
-            if (input > randomNumber) {
-                System.out.println("Your guess is to high!");
-                System.out.println("Try Again");
-            } else if (input < randomNumber) {
-                System.out.println("your guess is too low!");
-                System.out.println("Try Again");
-            }
-            guess++;
-            if(guess==6){
-                System.out.println("You failed to get it right in 6 turns");
-                System.out.println("You lose.");
-                break;
-            }
+        int input=0;
+        try {
             input = getInput.nextInt();
+        }catch(Exception e){
+            System.out.println("Caught Exception: Input not a number");
         }
-        if(input==randomNumber) {
-            System.out.println("You got it right!");
-        }
-
+            while (input != randomNumber) {
+                if (input > randomNumber && input < 21) {
+                    System.out.println("Your guess is to high!");
+                    System.out.println("Try Again");
+                } else if (input < randomNumber && input > 0) {
+                    System.out.println("your guess is too low!");
+                    System.out.println("Try Again");
+                }
+                guess++;
+                if (guess == 6) {
+                    System.out.println("You failed to get it right in 6 turns");
+                    System.out.println("You lose.");
+                    break;
+                }
+                try {
+                    input = getInput.nextInt();
+                }catch(Exception e){
+                    System.out.println("Caught Exception: Input not a number");
+                }
+            }
+            if (input == randomNumber) {
+                System.out.println("You got it right!");
+            }
     }
 }
